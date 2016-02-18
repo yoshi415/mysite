017b7544508c3ea2cd65b50d1f0726c941c5f3ea
Yoshio Varney <yoshio.varney@gmail.com>
2016-02-17 17:20:32 -0800

    Completed page transitions

--- source/main.js
+++ source/main.js
@@ -1,10 +1,31 @@
 $(() => {
-  $('.main').onepage_scroll({
+  const $main = $('.main');
+  const $body = $('body');
+  let direction = false;
+
+  $main.onepage_scroll({
         easing: 'ease-in-out',
-        animationTime: 700,
+        animationTime: 1000,
+        // beforeMove: changeColor,
+        afterMove: changeColor,
         pagination: true,
         loop: false,
         keyboard: true,
         responsiveFallback: 600
-      });
+  });
+
+  $('#up').click(() => {
+      $main.moveUp();
+  });
+  $('#down').click(() => {
+      $main.moveDown();
+  });
+
+  function changeColor() {
+    direction = !direction;
+    let colors = direction ? ['firstColor', 'secondColor'] : [ 'secondColor', 'firstColor' ];
+    $body.removeClass(colors[0]).addClass(colors[1]);
+  }
+
+
 });
